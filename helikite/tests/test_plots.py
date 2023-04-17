b778d1a988ac118e0137ed36be02bd7ddbc1eaa2
Evan Thomas <evan.thomas@epfl.ch>
2023-04-17 12:41:12 +0200

    Add campaign data for unit tests, add unit test for NaN values in float NaNs for colourmap generator

---
+++ helikite/tests/test_plots.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import pytest
+import os
+import sys
+from io import StringIO
+import datetime
+
+# Append the root directory of your project to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from instruments.smart_tether import SmartTether, smart_tether
+from instruments.flight_computer import flight_computer
+from plots import generate_normalised_colours
+
+CAMPAIGN_DATA = os.path.join(
+    os.path.dirname(__file__), "resources", "campaigns", "20220929")
+
+
+@pytest.fixture
+def fc_data():
+    # Import flight computer data from the campaign data folder
+
+    flight_computer.filename = os.path.join(CAMPAIGN_DATA, "LOG_20220929.txt")
+    df = flight_computer.read_data()
+
+    return df
+
+def test_nan_for_colourmap(fc_data: pd.DataFrame):
+    ''' Test that NaN values do not break the colourmap generator '''
+
+    pd.set_option('mode.chained_assignment', None)  # Disable copy slice warning
+
+    # Convert the TEMP2 column to float
+    fc_data['TEMP2'] = fc_data['TEMP2'].astype(float)
+
+    # Add some nan values to the TEMP2 column
+    fc_data['TEMP2'][0] = float('nan')
+    fc_data['TEMP2'][1] = float('nan')
+
+    # Assert there are nan values in the TEMP2 column
+    assert fc_data['TEMP2'].isnull().values.any() == True, (
+        "There are no NaN values to test with")
+
+    # Execute function to ensure it does not raise ValueError
+    generate_normalised_colours(fc_data)
