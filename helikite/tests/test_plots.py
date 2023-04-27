99cad45d16e2a5d6223eafba1e415364c976b2bc
Evan Thomas <evan.thomas@epfl.ch>
2023-04-27 10:49:13 +0200

    Fix numpy boolean comparison in colourmap test

--- helikite/tests/test_plots.py
+++ helikite/tests/test_plots.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import sys
+import numpy
 
 # Append the root directory of your project to the system path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -20,7 +21,7 @@ def test_nan_for_colourmap(fc_data: pd.DataFrame):
     fc_data['TEMP2'][1] = float('nan')
 
     # Assert there are nan values in the TEMP2 column
-    assert fc_data['TEMP2'].isnull().values.any() is True, (
+    assert fc_data['TEMP2'].isnull().values.any() is numpy.bool_(True), (
         "There are no NaN values to test with")
 
     # Execute function to ensure it does not raise ValueError
